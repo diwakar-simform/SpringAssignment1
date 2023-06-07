b30e3a8b5e6c7aee1de459a8f9555710497ba773
Diwakar Kumar <diwakar@simformsolutions.com>
2023-06-07 10:21:32 +0530

    first commit

---
+++ springboot-demo/src/main/java/com/springassignment1/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.springassignment1.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("...............Demo SpringBootApplication.............");
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
